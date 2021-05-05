ec1e336599757ac71c2a8440a9095b6dd5e4ae39
shivpdesai <shivpdesai@gmail.com>
2021-05-05 00:05:05 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from vpython import *
+canvas(background= color.purple)
+donut= ring(radius=0.5,thickness=0.25,color= vector(400, 100, 1))
+coating= ring(radius=0.55,thickness=0.25,color= vector(0.4, 0.2, 0))
+rad = 0
+while True:
+    rate(10)
+    donut.pos = vector(3*cos(rad),sin(rad),0)
+    coating.pos = vector(3*cos(rad),sin(rad), 0)
+    rad = rad + 0.15
+
+
